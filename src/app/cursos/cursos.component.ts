a7624d2fe5e57bf64ebd58200fb9010db3c785f1
Pedro <pedroferreiracjr@gmail.com>
2021-09-04 18:42:30 -0300

    melhoria explicada na aula sobre a palavra-chave private no construtor

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -10,20 +10,22 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos!: string[];
-  _cursosService!: CursosService;
+
   // A forma correta de injeção de dependência é declarar uma dependência através do construtor.
   // No Angular as dependências são resolvidas através do construtor da classe dependente.
-  constructor(_cursosService: CursosService) {
+  // Uma ultima melhoria pode ser feita para tornar esse atributo privado neste componente,
+  //que é usando a palavra-chave 'private'.
+  constructor(private cursosService: CursosService) {
     // Primeiro exemplo de como obter uma dependência da instância do component.
     // Conforme discutido em aula, foi dito que esta não é a melhor forma de obteção das
     //dependências de qualquer objeto gerenciado pelo Angular.
     // Essa não é a forma correta de fazer a injeção de dependência.
     //this.cursosService = new CursosService();
     // A primeira maneira é recebendo e atribuíndo para um atributo do component
-    this._cursosService = _cursosService;
+    //this._cursosService = _cursosService;   // removido por conta do private
   }
 
   public ngOnInit(): void {
-    this.cursos = this._cursosService.getCursos();
+    this.cursos = this.cursosService.getCursos();
   }
 }
